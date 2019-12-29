3fe3149eddda4b84c96c9c29be450e40d4b1f0f1
John Doe <randomEme@protonmail.com>
2019-12-29 14:11:56 +0400

    Начал делать todo list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListTodo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListTodo from "@/components/ListTodo";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ListTodo    
   }
 }
 </script>
---
+++ src/components/ListTodo.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="list-todo">
+    <li class="list-todo__item">3</li>
+    <li class="list-todo__item">2</li>
+    <li class="list-todo__item">1</li>
+  </ul>
+</template>
